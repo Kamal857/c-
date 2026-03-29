f01ca648e1f97f7fe6ffd7fa733e725ce0b469f8
Kamal Bohara <boharakamal857@gmail.com>
2026-03-29 23:06:18 +0545

    first day program

---
+++ day1.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<string.h>
+using namespace std;
+int main()
+{
+    int a;
+    cout<<"Enter a Number : ";
+    cin>>a;
+    cout<<"The input number is "<<a<<"!";
+    return 0;
+}
---
+++ main.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+    int a,b;
+    printf("Enter the two numbers : ");
+    scanf("%d%d",&a,&b);
+    printf("You Entered a= %d and b= %d ",a,b);
+    return 0;
+}
