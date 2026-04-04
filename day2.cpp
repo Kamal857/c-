64cf91ddafd9160db3e85cb88a38ad3a1d930362
Kamal Bohara <boharakamal857@gmail.com>
2026-04-04 21:46:40 +0545

    day 2

---
+++ day2.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int i;
+    char name[10];
+    for(i=0;i<2;i++)
+    {
+        cout<<"TEll Me your Name ?";
+        
+        cin>>name;
+
+    }
+    return 0;
+}
